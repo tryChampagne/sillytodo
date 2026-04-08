0ce94464e8b9e941b8ce5144c4fb1542ce70238d
shivam <tle.really.sucks@gmail.com>
2026-04-08 17:45:31 +0530

    Feat: Add todo form UI ,allow users to add task

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { MAX_INPUT_LENGTH } from "../Constant";
+import styles from "./TodoForm.module.css";
+
+export default function TodoForm({ setTodoList }) {
+  const [triggerAnime, setTriggerAnime] = useState(0);
+  const [userInput, setUserInput] = useState("");
+  const isTooLongText = userInput.length > MAX_INPUT_LENGTH;
+
+  // Event handlers
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const trimmed = userInput.trim();
+
+    // Prevent users from submitting only spaces
+    if (trimmed.length === 0) {
+      setTriggerAnime((prev) => ++prev);
+    } else {
+      addTask(trimmed);
+    }
+    setUserInput("");
+  };
+
+  // Features
+  const addTask = (trimmed) => {
+    setTodoList((prev) => {
+      const niceText = trimmed.charAt(0).toUpperCase() + trimmed.slice(1);
+      return [...prev, niceText];
+    });
+  };
+  return (
+    <div className={styles.form_container}>
+      {/* Animated warning , when user submit only spaces */}
+      {triggerAnime > 0 && (
+        <div key={triggerAnime} className={styles.only_spaces}>
+          Don't be rude!
+        </div>
+      )}
+
+      <form
+        className={` ${styles.form} ${isTooLongText ? styles.warn_animation : ""}`}
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          value={userInput}
+          onChange={(e) => {
+            setUserInput(e.target.value);
+          }}
+          placeholder="Enter your task..."
+          required
+          autoFocus
+          className={styles.user_input}
+        />
+
+        <input
+          type="submit"
+          value={"+"}
+          className={styles.btn_submit}
+          disabled={isTooLongText}
+        />
+      </form>
+
+      {/* Too long text warning! */}
+      {isTooLongText && (
+        <div className={styles.form_warning}>
+          {" "}
+          <svg
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M12 3L2 21H22L12 3Z" fill="#facc15" />
+            <path
+              d="M12 9V13"
+              stroke="#000"
+              strokeWidth={2}
+              strokeLinecap="round"
+            />
+            <circle cx={12} cy={17} r={1} fill="#000" />
+          </svg>
+          <strong>Text is too long</strong>{" "}
+        </div>
+      )}
+    </div>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,34 @@
+import { useEffect, useState } from "react";
 import styles from "./HomePage.module.css";
+import TodoForm from "../components/TodoForm";
+
 export default function HomePage() {
+  // States
+  const [todoList, setTodoList] = useState(() => {
+    try {
+      const stored = JSON.parse(localStorage.getItem("todo")) ?? [];
+      if (!Array.isArray(stored)) throw new Error("Only arrays are allowed!");
+      return stored;
+    } catch {
+      return [];
+    }
+  });
+
+  // useEffects
+  useEffect(() => {
+    localStorage.setItem("todo", JSON.stringify(todoList));
+  }, [todoList]);
+
+  // JSX
   return (
     <div className={styles.home_page_wrapper}>
+      {/* Header */}
       <div className={styles.header_container}>
-        <h1 className={styles.header}> Homepage</h1>
+        <h1 className={styles.header}> TodoList</h1>
       </div>
 
-      <div className={styles.msg_container}>
-        <p className={styles.msg}>Привет!</p>
-      </div>
+      {/* Body */}
+      <TodoForm setTodoList={setTodoList} />
     </div>
   );
 }
